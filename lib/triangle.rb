87a40d966250de0d779fa8feb1e8868ca5c49a5c
Brendan O'Neill <oneill.brendan88@gmail.com>
2019-01-20 22:21:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,6 +14,7 @@ class Triangle
     @side_b = side_b 
     @side_c = side_c 
     
+<<<<<<< HEAD
     if side_a == 0 && side_b == 0 && side_c == 0 
      @kind = :invalid
     
@@ -34,6 +35,35 @@ class Triangle
   
 
   end
+=======
+    
+    side_a == 0 && side_b == 0 && side_c = 0
+    if 
+      @kind = :invalid 
+    elsif side_a == side_b || side_b == side_c || side_c == side_a 
+      @kind = :isosceles
+    elsif side_a != side_b && side_b != side_c && side_c != side_a 
+      @kind = :scalene
+    elsif side_a == side_b && side_b == side_c
+      @kind = :equilateral
+  end
+end
+
+ def kind
+    if @kind == :invalid
+      raise TriangleError 
+    else 
+    @kind 
+  end 
+end
+  
+
+
+
+
+ 
+
+>>>>>>> bc4090c6104a1163c12412866f1a42c3cc464c78
 end
 
  def kind
